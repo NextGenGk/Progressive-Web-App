b529e332186470e05141a464a917a5ff0d20fd92
GAURAV KUMAR <atkola12345@gmail.com>
2024-12-06 00:58:39 +0530

    first commit

---
+++ server-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'tic-tac-toe-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/tic-tac-toe.css',
+  '/js/tic-tac-toe.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache first, then from network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
